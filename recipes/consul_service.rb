8b66f49be9742493b7bc22210b3b063827205421
Andrew Shieh <ishieh@roblox.com>
2020-07-10 11:38:32 -0700

    add tag for pod

--- recipes/consul_service.rb
+++ recipes/consul_service.rb
@@ -8,20 +8,27 @@ port = 9090
 consul_name = "prometheus-pod-telemetry"
 
 tag_list = ['prometheus']
-meta = {'path' => 'api/v1/metrics/prometheus'}
+infradb_available = (node.key?('infradb') and node['infradb'].key?('serverInfo') and node['infradb']['serverInfo'].key?('Server') and not node['infradb']['serverInfo']['Server'].nil?)
+location_available = (not node['infradb']['serverInfo']['Server']['Location'].nil?)
+
+if infradb_available && location_available
+  pod = node['infradb']['serverInfo']['Server']['Location']['Pod']['Name']
+  tag_list << pod
+  skip_update = node['rblx_prometheus']['skip_consul']
+else
+  skip_update = true
+end
 
 consul_definition consul_name do
   type 'service'
   parameters(
     name: consul_name,
-    address: '127.0.0.1',
     port: port,
-    tags: tag_list,
-    meta: meta,
+    tags: tag_list
   )
   notifies :reload, 'consul_service[consul]', :delayed
   not_if { node['rblx_prometheus']['absent_consul'] }
-  not_if { node['rblx_prometheus']['skip_consul'] }
+  not_if { skip_update }
 end
 
 consul_definition consul_name do
